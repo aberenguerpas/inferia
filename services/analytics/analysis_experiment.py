257113d086f03a0770218cb495b49e77f1413fe0
Alberto Berenguer Pastor <alberto.berenguer@ua.es>
2022-12-14 10:38:46 +0100

    Add exploratory analysis script

--- services/analytics/analysis_experiment.py
+++ services/analytics/analysis_experiment.py
@@ -11,16 +11,19 @@ def main():
     args = parser.parse_args()
 
     list_files = glob.glob(os.path.join(args.input, '*.csv'))
-    table = pd.read_csv(list_files.pop(0), index_col='Name')  # Store the first file
-    for file in list_files:
-        new_table = pd.read_csv(file, index_col='Name')
-        table = pd.concat([table, new_table], axis=0)  # Add the rows of new the new table to the previous one
-    # table.to_csv('salida.csv')
-    pd.set_option('display.max_columns', None)  # Show all the column values
-    print('All:')
-    print(table.describe())
-    print('Mean:')
-    print(table.mean())
+    try:
+        table = pd.read_csv(list_files.pop(0), index_col='Name')  # Store the first file
+        for file in list_files:
+            new_table = pd.read_csv(file, index_col='Name')
+            table = pd.concat([table, new_table], axis=0)  # Add the rows of new the new table to the previous one
+        # table.to_csv('salida.csv')
+        pd.set_option('display.max_columns', None)  # Show all the column values
+        print('All:')
+        print(table.describe())
+        print('Mean:')
+        print(table.mean())
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
---
+++ services/analytics/exploratory_analysis.py
@@ -0,0 +1,32 @@
+import argparse
+import glob
+import pandas as pd
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Corpus Exploratory Analysis')
+    parser.add_argument('-i', '--input', required=True,
+                        help='Name of the input folder storing the corpus CSV files ')
+    args = parser.parse_args()
+
+    list_files = glob.glob(os.path.join(args.input, '*.csv'))
+
+    data = []
+    for file in list_files:
+        df = pd.read_csv(file)
+
+        data.append([len(df.index),len(df.columns)])
+        #table = pd.concat([table, new_table], axis=0)  # Add the rows of new the new table to the previous one
+ 
+    df_result = pd.DataFrame(data=data ,columns=['n_rows', 'n_columns'])
+
+    pd.set_option('display.max_columns', None)  # Show all the column values
+    print('All:')
+    print(df_result.describe())
+    print('Mean:')
+    print( df_result.mean())
+
+
+if __name__ == '__main__':
+    main()
